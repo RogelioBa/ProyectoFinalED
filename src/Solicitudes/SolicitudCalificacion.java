24ae2fd639e23edfab91e0111fd8df1009dcbbd1
jualm <jualm@Juan>
2025-12-03 18:38:46 -0700

    Persona 2 completo

--- src/Solicitudes/SolicitudCalificacion.java
+++ src/Solicitudes/SolicitudCalificacion.java
@@ -5,7 +5,8 @@
 package Solicitudes;
 
 /**
- *
+ * Representa una solicitud para agregar o modificar una calificación.
+ * Puede incluir un índice (modificación) o no (inserción).
  * @author axelm
  */
 public class SolicitudCalificacion {
@@ -13,12 +14,25 @@ public class SolicitudCalificacion {
     private float calificacion;
     private Integer indice;
 
+    /**
+     * Crea una solicitud de inserción de calificación.
+     *
+     * @param matricula matrícula del estudiante
+     * @param calificacion valor a insertar
+     */
     public SolicitudCalificacion(String matricula, float calificacion) {
         this.matricula = matricula;
         this.calificacion = calificacion;
         this.indice = null;
     }
 
+    /**
+     * Crea una solicitud de modificación de calificación.
+     *
+     * @param matricula matrícula del estudiante
+     * @param calificacion nuevo valor
+     * @param indice posición a modificar
+     */
     public SolicitudCalificacion(String matricula, float calificacion, int indice) {
         this.matricula = matricula;
         this.calificacion = calificacion;
